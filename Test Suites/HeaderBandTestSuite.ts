95d5a0a1b4de9d002a574516df42a9ef52212041
Deepthinvs <anvs.deepthi@gmail.com>
2021-02-05 14:23:35 -0600

    ATTScreening

---
+++ Test Suites/HeaderBandTestSuite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>HeaderBandTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>555b12da-9422-4b38-9659-833ed43a3818</testSuiteGuid>
+   <testCaseLink>
+      <guid>d685ddd0-e935-4ada-acba-ba6cd0921b1b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DealsTabTestCase</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a09db6e7-8436-45df-8b1b-f663b7d38b7f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SearchBar_TestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/MenuBar TestSUit.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MenuBar TestSUit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>098c4a5f-72dc-422d-9ca3-85387776982f</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ff378fb-e772-4427-a44b-81a33b509b5f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Menu_TestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
